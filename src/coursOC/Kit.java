8e6743c12a53ee9671512e21bbf55313aa8a1dfa
Tatiana <tatiana.allery@orange.fr>
2025-02-13 18:25:54 +0100

    fix: kit not using generics but java.util List and Set

--- src/coursOC/Kit.java
+++ src/coursOC/Kit.java
@@ -1,21 +1,30 @@
 package coursOC;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Kit {
-	private ListOfElementForKit<Block> blocks;
-	private ListOfElementForKit<String> keywords;
+	private List<Block> blocks = new ArrayList<Block>();
+	private Set<String> keywords = new LinkedHashSet<String>();
 	
 	public Kit() {
-		ListOfElementForKit<Block> blocks = new ListOfElementForKit<Block>(Block.class, 5);
-		blocks.addElementInList(0, new Wall(true, 1, 1, 1));
-		blocks.addElementInList(1, new Wall(true, 1, 1, 1));
-		blocks.addElementInList(2, new Wall(true, 1, 1, 1));
-		blocks.addElementInList(3, new Wall(true, 1, 1, 1));
-		blocks.addElementInList(4, new Door(false, 1, 1, 1));
+		blocks.add(0, new Wall(true, 3, 2, 2));
+		blocks.add(1, new Wall(true, 3, 2, 2));
+		blocks.add(2, new Wall(false, 2, 1, 2));
+		blocks.add(3, new Wall(false, 2, 1, 2));
+		blocks.add(4, new Door(true, 1, 2, 2));
 		
-		ListOfElementForKit<String> keywords = new ListOfElementForKit<String>(String.class, 2);
-		keywords.addElementInList(0, "cabane");
-		keywords.addElementInList(1, "muraille");
+		keywords.add("cabane");
+		keywords.add("muraille");
+	}
+	
+	public void showKit() {
+		System.out.println("Nombre de blocs dans le kit :" + blocks.size());
+		for(String keyword : keywords) {
+			System.out.println(keyword);
+		}
 	}
 	
 }
--- src/coursOC/Main.java
+++ src/coursOC/Main.java
@@ -3,7 +3,8 @@ package coursOC;
 public class Main {
 
 	public static void main(String[] args) {
-
+		Kit kit = new Kit();
+		kit.showKit();
 	}
 	
 }
